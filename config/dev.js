aef91d73c7b9d351e6655f4573c2af3939b5f21d
Vivek Lakhanpal <lakhan_vivek@yahoo.com>
2019-09-05 11:35:31 -0400

    Update dev.js
    removed mongodb username/password

--- config/dev.js
+++ config/dev.js
@@ -2,7 +2,7 @@ module.exports = {
   googleClientID:
     '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
   googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
-  mongoURI: 'mongodb://viveknodejs:UnPj4jDarL$x#-t@nodeadv-shard-00-00-5by6h.mongodb.net:27017,nodeadv-shard-00-01-5by6h.mongodb.net:27017,nodeadv-shard-00-02-5by6h.mongodb.net:27017/test?ssl=true&replicaSet=nodeadv-shard-0&authSource=admin&retryWrites=true&w=majority',
+  mongoURI: 'mongodb://@nodeadv-shard-00-00-5by6h.mongodb.net:27017,nodeadv-shard-00-01-5by6h.mongodb.net:27017,nodeadv-shard-00-02-5by6h.mongodb.net:27017/test?ssl=true&replicaSet=nodeadv-shard-0&authSource=admin&retryWrites=true&w=majority',
   cookieKey: '123123123',
   redisUrl: 'redis://127.0.0.1:6379'
 };

650bc62f17e452e91e4a989303d93767e7d65591
Vivek Lakhanpal <vivek.lakhanpal@scotiabank.com>
2019-09-04 22:11:18 -0400

    First commit

--- config/dev.js
+++ config/dev.js
@@ -2,6 +2,7 @@ module.exports = {
   googleClientID:
     '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
   googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
-  mongoURI: 'mongodb://readonly:password@ds063124.mlab.com:63124/blog_everyone',
-  cookieKey: '123123123'
+  mongoURI: 'mongodb://viveknodejs:UnPj4jDarL$x#-t@nodeadv-shard-00-00-5by6h.mongodb.net:27017,nodeadv-shard-00-01-5by6h.mongodb.net:27017,nodeadv-shard-00-02-5by6h.mongodb.net:27017/test?ssl=true&replicaSet=nodeadv-shard-0&authSource=admin&retryWrites=true&w=majority',
+  cookieKey: '123123123',
+  redisUrl: 'redis://127.0.0.1:6379'
 };
